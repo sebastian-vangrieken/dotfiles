8ba9c3b62b1739e08539a99073e13639bc94eca2
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2025-02-28 20:42:09 +0100

    basic lsp for C/Cpp/rust, treesitter, mini-pairs, options, keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 -- Options --
-opt = vim.opt
+vim.cmd("let g:netrw_liststyle = 3")
+local opt = vim.opt
 
 opt.autoindent = true
 opt.background = "dark"
@@ -7,6 +8,7 @@ opt.clipboard = "unnamedplus"
 opt.cmdheight = 0
 opt.confirm = true
 opt.cursorline = true
+opt.expandtab = true
 opt.ignorecase = true
 opt.number = true
 opt.relativenumber = true
@@ -19,11 +21,12 @@ opt.wrap = true
 -- Keymaps --
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-map = vim.keymap
+local keymap = vim.keymap.set
 
-map.set("n", "<leader>q", ":q<cr>", { desc = "Quit" })
-map.set("n", "<leader>w", ":w<cr>", { desc = "Save" })
-map.set("n", "<leader>nh", ":noh<cr>", { desc = "Clear search highlight" })
+keymap("n", "<leader>q", ":q<cr>", { desc = "Quit" })
+keymap("n", "<leader>w", ":w<cr>", { desc = "Save" })
+keymap("n", "<leader>nh", ":noh<cr>", { desc = "Clear search highlight" })
+keymap("n", "<leader>fe", ":Ex<cr>", { desc = "Go to file explorer" })
 
 -- Auto commands --
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -51,7 +54,92 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {},
-	checker = { enabled = true },
-	change_detection = { notify = false },
+	spec = {
+		{
+			"folke/tokyonight.nvim",
+			lazy = false,
+			priority = 1000,
+			config = function()
+				vim.cmd[[colorscheme tokyonight-storm]]
+			end
+		},
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.8",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"nvim-tree/nvim-web-devicons",
+			},
+			config = function()
+				local actions = require("telescope.actions")
+				require("telescope").setup({
+					defaults = {
+						mappings = {
+							i = {
+								["<C-j>"] = actions.move_selection_next,
+								["<C-k>"] = actions.move_selection_previous,
+							},
+						},
+					},
+				})
+				local builtin = require("telescope.builtin")
+				vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files"})
+			end
+		},
+        {
+            "echasnovski/mini.pairs",
+            event = "VeryLazy",
+            config = function()
+                require("mini.pairs").setup({
+                    modes = { insert = true, command = false, terminal = false },
+                    mappings = {
+                        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+                        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+                        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+                        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\]." },
+                        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\]." },
+                        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\]." },
+                    },
+                })
+            end
+        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                    ensure_installed = {
+                        "bash",
+                        "c",
+                        "cpp",
+                        "cmake",
+                        "lua",
+                        "rust"
+                    },
+                    sync_installed = true,
+                    auto_install = true,
+                })
+            end
+        },
+        {
+            "neovim/nvim-lspconfig",
+            config = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.clangd.setup({})
+                lspconfig.rust_analyzer.setup({})
+                local on_attach = function(_,bufnr)
+                    local keymap = vim.api.nvim_buf_set_keymap
+                    local opts = { noremap = true, silent = true }
+                    keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+                    keymap(bufnr, "n", "<leader>I", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+                end
+                lspconfig.clangd.setup({ on_attach = on_attach })
+                lspconfig.rust_analyzer.setup({ on_attach = on_attach })
+            end
+        },
+    	checker = { enabled = true },
+	    change_detection = { notify = false },
+    }
 })
