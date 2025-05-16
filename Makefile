b37155a661ff67b8932beb1f2416f82a7a1fc9b0
Sebastian Van Grieken <sebastian.vangrieken12@gmail.com>
2025-05-16 14:20:16 +0200

    config files in config dir, automation/install scripts in install dir, Makefile can run everything or partial things, only basic for now

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: all symlinks install
+
+all: symlinks
+
+symlinks:
+	bash install/symlinks.sh
---
+++ install/symlinks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "Creating symlinks..."
+
+ln -s "$HOME/dotfiles/config/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/dotfiles/config/nvim" "$HOME/.config/nvim"
+ln -s "$HOME/dotfiles/config/kitty" "$HOME/.config/kitty"
+ln -s "$HOME/dotfiles/config/hypr" "$HOME/.config/hypr"
+
+echo "Symlinks created!"
