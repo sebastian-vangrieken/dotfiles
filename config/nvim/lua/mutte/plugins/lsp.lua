966e2685b6877f9f70b6a96f4982fef1fa1d4e88
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2025-06-28 20:52:15 +0200

    inline warnings/errors

--- config/nvim/lua/mutte/plugins/lsp.lua
+++ config/nvim/lua/mutte/plugins/lsp.lua
@@ -1,44 +1,51 @@
 return {
 	{
 		"mason-org/mason.nvim",
-		opts = {}
+		opts = {},
 	},
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = {},
 		config = function()
-			vim.lsp.config('lua_ls', {
+			vim.lsp.config("lua_ls", {
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
-						if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+						if
+							path ~= vim.fn.stdpath("config")
+							and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
 						then
 							return
 						end
 					end
 
-					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 						runtime = {
-							version = 'LuaJIT',
+							version = "LuaJIT",
 							path = {
-								'lua/?.lua',
-								'lua/?/init.lua',
+								"lua/?.lua",
+								"lua/?/init.lua",
 							},
 						},
 						workspace = {
 							checkThirdParty = false,
 							library = {
-								vim.env.VIMRUNTIME
-							}
-						}
+								vim.env.VIMRUNTIME,
+							},
+						},
 					})
 				end,
 				settings = {
-					Lua = {}
-				}
+					Lua = {},
+				},
 			})
-			vim.lsp.enable('phpactor')
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+			})
 		end,
 	},
 	{
@@ -52,7 +59,9 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"clangd",
-					"cssls", "css_variables", "cssmodules_ls",
+					"cssls",
+					"css_variables",
+					"cssmodules_ls",
 					"eslint",
 					"html",
 					"lua_ls",
