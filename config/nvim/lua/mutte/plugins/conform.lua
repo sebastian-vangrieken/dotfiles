51120d657277a5a8014b13f45db08e48af9d95f3
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2025-07-02 14:51:20 +0200

    format c using 4 spaces

--- config/nvim/lua/mutte/plugins/conform.lua
+++ config/nvim/lua/mutte/plugins/conform.lua
@@ -13,6 +13,12 @@ return {
 				php = { "pretty-php", "php-cs-fixer" },
 				typescript = { "prettier" },
 			},
+			formatters = {
+				["clang-format"] = {
+					command = "clang-format",
+					args = { "--style={IndentWidth: 4}" },
+				},
+			},
 			format_on_save = {
 				timeout_ms = 500,
 				lsp_format = "fallback",
