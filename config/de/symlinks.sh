740c14d29903eabb0be4d75f54747ad9af03d6fd
sebastian-vangrieken <sebastian.vangrieken12@gmail.com>
2025-10-01 16:23:15 +0200

    alacritty instead of kitty, hyprland config tweaks, starting on waybar config

--- config/de/symlinks.sh
+++ config/de/symlinks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ln -s ~/dotfiles/config/de/hypr ~/.config/hypr
-ln -s ~/dotfiles/config/de/kitty ~/.config/kitty
+ln -s ~/dotfiles/config/de/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/config/de/waybar ~/.config/waybar
+ln -s ~/dotfiles/config/de/wofi ~/.config/wofi
